c75842fc4b39eb29f79b945cceba0fdf7bb8b631
David Szklarzewski <dszklarz@fu-r6-p4.s19.be>
2021-11-04 12:15:22 +0100

    prompt

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME	= minishell
+
+SRCDIR	= src
+
+SRCS	= $(shell find $(SRCDIR)/*.c)
+
+OBJDIR	= obj
+
+OBJS	= $(subst $(SRCDIR), $(OBJDIR), $(SRCS:.c=.o))
+
+INCDIR	= inc
+
+CC		= gcc
+
+RM		= rm -f
+
+CFLAGS	= -Wall -Werror -Wextra
+
+LIB		= -lreadline
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.c
+			$(CC) $(CFLAGS) -I$(INCDIR) -c $^ -o $@
+                
+all:	$(NAME)
+
+$(OBJDIR):
+			mkdir -p $(OBJDIR) 
+
+$(NAME):        $(OBJDIR) $(OBJS)
+			$(CC) $(CFLAGS)  $(LIB) $(OBJS) -o $(NAME)
+
+install:
+			mkdir $(OBJDIR) $(SRCDIR) $(INCDIR)
+			touch $(INCDIR)/$(NAME).h
+			touch $(SRCDIR)/$(NAME).c
+clean:
+			$(RM) -r $(OBJDIR)
+
+fclean:	clean
+			$(RM) $(NAME)
+
+re:	fclean all
+
+.PHONY:	all clean fclean re install
---
+++ inc/minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dszklarz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 12:07:39 by dszklarz          #+#    #+#             */
+/*   Updated: 2021/11/04 12:10:05 by dszklarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# define PROMPT "minishell>"
+
+/*includes*/
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+/*lexer*/
+
+/*parser*/
+
+/*expander*/
+
+/*executor*/
+
+/*functions*/
+
+/*errors*/
+
+
+#endif
