9c11398d5d746d85e9212abe77a7629f343e56b6
Martin Lefevre <lefevre.martin@outlook.com>
2021-11-06 21:44:18 +0100

    made expand_variables compilable

--- src/expand_variables.c
+++ src/expand_variables.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   replace_env.c                                      :+:      :+:    :+:   */
+/*   expand_variables.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlefevre <mlefevre@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 19:58:35 by mlefevre          #+#    #+#             */
-/*   Updated: 2021/11/06 20:58:59 by mlefevre         ###   ########.fr       */
+/*   Updated: 2021/11/06 21:36:28 by mlefevre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
+/*
+Returns value of environment
+variable name in envp NULL in
+case of error.
+*/
 char	*get_envp_val(const char *name, char **envp)
 {
 	int	l;
@@ -25,15 +31,28 @@ char	*get_envp_val(const char *name, char **envp)
 	while (*envp)
 	{
 		if (!ft_strncmp(name, *envp, l) && (*envp)[l] == '=')
-			return (ft_substr(*envp, l + 1, ft_strlen(*envp + l + 1));
-		*envp++;
+			return (ft_substr(*envp, l + 1, ft_strlen(*envp + l + 1)));
+		envp++;
 	}
 	return (0);
 }
 
+/*
+Note to self:
+$VAR shall expand to value of VAR.
+$VAR shall be removed if VAR not found.
+$ followed by a space shall NOT be removed.
+*/
+/*
+Returns new string with all variables
+in s expanded. NULL in case of error.
+(envp for exported variables, locals for the rest)
+*/
 char	*expand_variables(const char *s, char **envp, char **locals)
 {
+	(void)envp;
+	(void)locals;
 	if (!s)
 		return (0);
-	
+	return (0);
 }
