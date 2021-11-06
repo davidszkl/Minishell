185a3e1c3800d1c0cd1fdbfbaf445b747984bf57
Martin Lefevre <lefevre.martin@outlook.com>
2021-11-06 21:43:17 +0100

    added ft_strdup.c and modified ft_substr.c

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -1,38 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlefevre <mlefevre@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/05 11:02:44 by mlefevre          #+#    #+#             */
-/*   Updated: 2021/11/06 21:41:13 by mlefevre         ###   ########.fr       */
+/*   Created: 2021/10/05 11:01:57 by mlefevre          #+#    #+#             */
+/*   Updated: 2021/11/06 21:41:57 by mlefevre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *s1);
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strdup(const char *s1)
 {
-	char	*r;
-	size_t	i;
+	const size_t	l = ft_strlen(s1);
+	char			*s;
 
+	s = malloc(l + 1);
 	if (!s)
 		return (0);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if (len > ft_strlen(&s[start]))
-		len = ft_strlen(&s[start]);
-	r = malloc(len + 1);
-	if (!r)
-		return (0);
-	i = -1;
-	while (++i < len)
-		r[i] = s[start + i];
-	r[i] = 0;
-	return (r);
+	while (*s1)
+		*s++ = *s1++;
+	*s = 0;
+	return (s - l);
 }
