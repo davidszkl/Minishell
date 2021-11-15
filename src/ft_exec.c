8c49f5f9b7bd9bbf08558407df450d26db06e694
mlefevre <lefevre.martin@outlook.com>
2021-11-15 18:12:08 +0100

    started ft_exec nothing really done

--- src/ft_exec.c
+++ src/ft_exec.c
@@ -6,13 +6,24 @@
 /*   By: dszklarz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 09:30:55 by dszklarz          #+#    #+#             */
-/*   Updated: 2021/11/08 09:30:56 by dszklarz         ###   ########.fr       */
+/*   Updated: 2021/11/15 18:10:51 by mlefevre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/minishell.h"
 
-/* void	ft_exec(void)
+static size_t	get_comm_num(t_comm *cline)
 {
-	return ;
+	size_t	i;
+
+	i = 0;
+	while (cline[i].line)
+		i++;
+	return (i);
 }
- */
+
+void	ft_exec(t_main *main)
+{
+	const size_t	n = get_comm_num(main->cline);
+	(void)n;
+}
+
